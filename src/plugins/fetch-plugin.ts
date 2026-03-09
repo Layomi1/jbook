7283d7569d90767f67a5dd8db2d5b01bc0a1ecf6
Layomi <layomilawore@gmail.com>
2026-03-09 15:15:46 +0100

    tricking esbuild css handling by wrap the css in Js and uploading into DOM

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,18 +10,52 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /(^index\.tsx$)/ }, () => {
+        return {
+          loader: "jsx" as esbuild.Loader,
+          contents: inputCode,
+        };
+      });
+
+      build.onLoad(
+        { filter: /css$/ },
+        async (args: esbuild.OnLoadArgs): Promise<esbuild.OnLoadResult> => {
+          // check to see if file has been fetched,if yes, return file immediately
+          const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+            args.path,
+          );
+          if (cachedResult) return cachedResult;
+
+          // if no, fetch request database,fetch request database,
+          const { data, request } = await axios.get(args.path);
+
+          const escaped = data
+            .replace(/\n/g, "")
+            .replace(/"/g, '\\"')
+            .replace(/'/g, "\\'");
+
+          const contents = `
+                const style = document.createElement('style');
+                style.innerText = '${escaped}';
+                document.head.appendChild(style);
+              `;
+
+          // store response in cache,then send the user
+          const result: esbuild.OnLoadResult = {
+            loader: "jsx" as esbuild.Loader,
+            contents,
+            resolveDir: new URL("./", request.responseURL).pathname,
+          };
+
+          await fileCache.setItem(args.path, result);
+          return result;
+        },
+      );
+
       build.onLoad(
         { filter: /.*/ },
         async (args: esbuild.OnLoadArgs): Promise<esbuild.OnLoadResult> => {
-          if (args.path === "index.tsx") {
-            return {
-              loader: "jsx" as esbuild.Loader,
-              contents: inputCode,
-            };
-          }
-
           // check to see if file has been fetched,if yes, return file immediately
-
           const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
             args.path,
           );
